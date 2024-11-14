dabf1744fdd899a8ac5c2bb4c8b44074e13a9356
dannysimo <146620876+dannysimo@users.noreply.github.com>
2024-11-14 13:21:33 +0200

    Delete painter.hpp

--- painter.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "abstract_painter.hpp"
-
-class Painter : public AbstractPainter {
-public:
-    virtual void DrawImage(Point topLeft, Point bottomRight, char** image) override;
-    virtual void WriteText(Point position, char* text) override;
-};
